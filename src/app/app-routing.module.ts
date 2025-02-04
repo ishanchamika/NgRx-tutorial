fa1eb37bb1166a429002575a874d07b1276770d8
ishanchamika <ishanchami9@gmail.com>
2025-02-04 19:43:44 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'counter', pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './state/counter/counter.reducer';
+import { CounterComponent } from './components/counter/counter.component';
+import { TopNavComponent } from './components/layouts/top-nav/top-nav.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { EmployeeReducer } from './state/employee/employee.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { UsersComponent } from './components/users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { userReducer } from './state/user/user.reducer';
+import { UserEffects } from './state/user/user.effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    TopNavComponent,
+    EmployeeComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({counter: counterReducer, employees: EmployeeReducer, userState: userReducer}),
+    EffectsModule.forRoot([UserEffects])
   ],
   providers: [
     provideClientHydration(withEventReplay())
