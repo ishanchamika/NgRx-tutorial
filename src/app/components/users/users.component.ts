8b79ef07a0d58fc27c70e4a48cae61fbc5780667
ishanchamika <ishanchami9@gmail.com>
2025-02-05 23:25:00 +0530

    add remove user function

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { selectUsers } from '../../state/user/user.selector';
+import { selectUsers , selectLoading, selectLoaded, selectError, selectUserState} from '../../state/user/user.selector';
 import * as UserActions from '../../state/user/user.actions';
 import { Observable } from 'rxjs';
 import { Employee } from '../../models/employee.model';
@@ -8,18 +8,28 @@ import { Employee } from '../../models/employee.model';
 @Component({
   selector: 'app-users',
   standalone: false,
-  
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent implements OnInit
 {
-  // users$ = this.store.select(selectUsers)
   users$: Observable<ReadonlyArray<Employee>>;
+  loading$: Observable<boolean>;
+  loaded$: Observable<boolean>;
+  error$: Observable<any>;
+  
   constructor(private store:Store)
   {
     this.users$ = this.store.select(selectUsers);
+    this.loading$ = this.store.select(selectLoading);
+    this.loaded$ = this.store.select(selectLoaded);
+    this.error$ = this.store.select(selectError);
   }
+  removeUser(id: string)
+  {
+    this.store.dispatch(UserActions.removeUser({id}));
+  }
+  
   ngOnInit(): void 
   {
     this.store.dispatch(UserActions.loadUsers());
