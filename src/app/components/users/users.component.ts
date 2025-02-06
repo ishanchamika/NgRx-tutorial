85605e2b505436d5db4d2e5e6dfbcb0c8e7407af
ishanchamika <ishanchami9@gmail.com>
2025-02-06 23:46:04 +0530

    user component updated

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -5,6 +5,7 @@ import * as UserActions from '../../state/user/user.actions';
 import { Observable } from 'rxjs';
 import { Employee } from '../../models/employee.model';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-users',
@@ -25,6 +26,13 @@ export class UsersComponent implements OnInit
     this.loading$ = this.store.select(selectLoading);
     this.loaded$ = this.store.select(selectLoaded);
     this.error$ = this.store.select(selectError);
+  }
+
+  addUser()
+  {
+    const user:User = {id:"4", name:"Ishan Chamika", email:"ics@gmail.com", phone: "098485663"};
+    this.store.dispatch(UserActions.addUser({user}));
+
   }
   removeUser(id: string)
   {
